781c76b720b65fee2b5c1f449ea4408d2f146ea2
ShavezKhanDevOp <shavezkhan.devop@gmail.com>
2021-11-06 11:36:17 +0530

    made port changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 8080
+const port = process.env.port || 8080
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile('/index.html'))
 app.listen(port, () => console.log(`listening on localhost:${port}`))
