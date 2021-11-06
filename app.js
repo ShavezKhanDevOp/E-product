1ceda71c058bab59a9f7312ab2f7b95cdfd10816
ShavezKhanDevOp <shavezkhan.devop@gmail.com>
2021-11-06 11:26:13 +0530

    made change in app.js file

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const path = require('path');
 const app = express()
 const port = 8080
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile('/index.html')))
+app.get('/', (req, res) => res.sendFile('/index.html'))
 app.listen(port, () => console.log(`listening on localhost:${port}`))
