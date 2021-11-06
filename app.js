cc9a81cfcf62d1ada13283de6d52489acf5f0fe6
ShavezKhanDevOp <shavezkhan.devop@gmail.com>
2021-11-06 11:59:57 +0530

    small fix

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const app = express()
 const port = process.env.port || 8080;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile('/index.html'))
-app.listen(port, () => console.log(`listening on localhost:${port}`))
+app.listen(port, () => {
+    console.log(`Process Port:${process.env.port}`)
+    console.log(`listening on localhost:${port}`);
+})
